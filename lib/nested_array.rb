bbbbbeea98b13f28375805d5aca20f8ae8bb8f2e
Lisa Jung <lisahjung@gmail.com>
2020-03-23 21:42:09 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,81 +1,3 @@
-ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
-]
-
-CONVENTIONAL_PRODUCE = [
-  "Grapefruit",
-  "Pineapple",
-  "Oranges",
-  "Watermelon",
-  "Eggplant"
-]
-
-
-def assembled_matrix
-  produce_storage_room = [
-    CONVENTIONAL_PRODUCE,
-    ORGANIC_PRODUCE]
-  
-end  
-  
-  
-def sorted_matrix 
-  produce_storage_room = [
-    CONVENTIONAL_PRODUCE.sort,
-    ORGANIC_PRODUCE.sort]
-end
-  
-def matrix_lookup(matrix, row, column)
-  matrix[row][column]
-end
-
-def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] = new_value
-  matrix
-end
-
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
 # Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
@@ -142,4 +64,4 @@ end
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
 #end
-=end
+
