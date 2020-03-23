53f17f1e20e3e25d314df70874b99a596eb6bb66
Lisa Jung <lisahjung@gmail.com>
2020-03-23 21:35:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,3 +1,4 @@
+=begin
 # Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
@@ -69,4 +70,4 @@ end
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
-
+=end
